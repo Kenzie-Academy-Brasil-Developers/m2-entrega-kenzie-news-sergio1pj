2cab0ad2685cf4e26fad8928d160bd1baf0e27b4
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-08-24 11:11:57 -0300

    alterações css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import { ApiRequests } from "./requests.js";
 import { Render } from "./render.js";
 const news =  await ApiRequests.getAllNews();
-const main = document.querySelector("main");
-main.append(Render.renderNewsList(news));
+const sectionNews = document.querySelector(".news");
+sectionNews.append(Render.renderNewsList(news));
 

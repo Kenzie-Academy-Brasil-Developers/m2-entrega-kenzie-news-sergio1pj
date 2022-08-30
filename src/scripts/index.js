bab269f0fb87b3c9162bca678659cb5aa6a7dd0c
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-08-30 10:29:51 -0300

    feat: desktop layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,6 @@
 import { ApiRequests } from "./requests.js";
 import { Render } from "./render.js";
 const news =  await ApiRequests.getAllNews();
-console.log(news)
 const sectionNews = document.querySelector(".news");
 const top = document.querySelector(".top");
 top.appendChild(Render.renderNewsList(news.filter(item => item.id === 3)));

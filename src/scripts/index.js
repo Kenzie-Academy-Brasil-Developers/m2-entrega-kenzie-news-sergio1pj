3d148ca002757efba982f407e04bd28088615122
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-08-31 10:16:39 -0300

    fix: removendo sections

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,6 @@
 import { ApiRequests } from "./requests.js";
 import { Render } from "./render.js";
 const news =  await ApiRequests.getAllNews();
-const sectionNews = document.querySelector(".news");
-const top = document.querySelector(".top");
-top.appendChild(Render.renderNewsList(news.filter(item => item.id === 3)));
+const sectionNews = document.querySelector("main");
 sectionNews.appendChild(Render.renderNewsList(news));
 
